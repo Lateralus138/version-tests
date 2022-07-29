48b081061d71ddad5a348203a4a87ac8a68955e9
Lateralus138 <faithnomoread@yahoo.com>
2022-07-29 13:31:25 -0500

    First Version Test worflow run - 0.1.1659118143 0.1.1659118143 - Fri 29 Jul 2022 06:31:25 PM UTC

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+// -----------------------------------------------
+// 'version-tests'
+// Copyright © 2022
+// Ian Pride @ New Pride Software/Services
+// Fri 29 Jul 2022 13:30:06 CST
+// -----------------------------------------------
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "Hello, Main.cpp\n";
+  return 0;
+}
